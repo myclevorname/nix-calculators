ccda5eacc3f00b13f84326397fd6f533b8f3c275
Sam Connelly <myclevorname@gmail.com>
2024-11-15 20:42:14 -0500

    Add stable toolchain, add CEaShell

---
+++ pkgs/build-support/ce-toolchain-stable/default.nix
@@ -0,0 +1,11 @@
+{ ce-toolchain, fetchFromGitHub }:
+ce-toolchain.overrideAttrs {
+  name = "ce-toolchain-12.1";
+  src = fetchFromGitHub {
+    owner = "CE-Programming";
+    repo = "toolchain";
+    fetchSubmodules = true;
+    rev = "decfb1b1848bc782b8deab846694e636951a2971";
+    hash = "sha256-YNufixi27A+l042+CLJtYzYxHEXt73c6iv3m+XDht24=";
+  };
+}
---
+++ pkgs/ce-programs/ceashell/default.nix
@@ -0,0 +1,19 @@
+{ buildCEProgramStable, fetchFromGitHub, lib, python3, bash, ... }:
+buildCEProgramStable {
+  name = "ceashell";
+  src = fetchFromGitHub {
+    owner = "RoccoLoxPrograms";
+    repo = "CEaShell";
+    fetchSubmodules = true;
+    rev = "12a1b03a84f15de312558d712bb2cb64570aa880";
+    hash = "sha256-1xysRuoILO9D3mCikRLD5LaLQxj9PUnrngJUJBBIAD4=";
+  };
+
+  nativeBuildInputs = [ python3 bash ];
+  meta = {
+    homepage = "https://ceme.tech/t18820";
+    description = "CEaShell (pronounced like \"Seashell\") is a new shell for the TI-84 Plus CE.";
+    license = with lib.licenses; [ gpl3Only ];
+    maintainers = with lib.maintainers; [ clevor ];
+  };
+}

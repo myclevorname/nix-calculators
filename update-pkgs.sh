f7f97927b674efaa1f1a10e81a066764753b3bfa
Sam Connelly <myclevorname@gmail.com>
2025-03-01 05:02:15 -0500

    Add v19 rebase test

--- update-pkgs.sh
+++ update-pkgs.sh
@@ -9,6 +9,6 @@ for i in ce-toolchain gcc4ti; do
 	nix-update legacyPackages.x86_64-linux.$i --version=branch --flake || exit 1
 done
 nix-update legacyPackages.x86_64-linux.ce-toolchain-stable --flake || exit 1
-nix-update legacyPackages.x86_64-linux.llvm-ez80 --version=rebase-v15 --flake || exit 1
+nix-update legacyPackages.x86_64-linux.llvm-ez80 --version=rebase-v19 --flake || exit 1
 
 NIXPKGS_ALLOW_UNFREE=1 nix build .#checks.x86_64-linux.default --extra-experimental-features "nix-command flakes" --impure

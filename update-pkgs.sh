e6860b62fa4512a11c3a707a6e27c8b5b875686b
Sam Connelly <myclevorname@gmail.com>
2025-03-14 22:25:49 -0400

    Make update-pkgs run in parallel, ignore file
    Specifically after_update.sh

--- update-pkgs.sh
+++ update-pkgs.sh
@@ -1,13 +1,17 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p bash nix-update
 
+set -e
+
 for i in $(ls pkgs/ce-programs | grep -v "tiboyceTestROM" | grep -v "crossbunTestPack"); do
-	nix-update legacyPackages.x86_64-linux.CEPrograms.$i --version=branch --flake || exit 1
+	nix-update legacyPackages.x86_64-linux.CEPrograms.$i --version=branch --flake &
 done
 
 for i in ce-toolchain llvm-ez80 gcc4ti; do
-	nix-update legacyPackages.x86_64-linux.$i --version=branch --flake || exit 1
+	nix-update legacyPackages.x86_64-linux.$i --version=branch --flake &
 done
-nix-update legacyPackages.x86_64-linux.ce-toolchain-stable --flake || exit 1
+nix-update legacyPackages.x86_64-linux.ce-toolchain-stable --flake &
+
+wait -f
 
 nix build .#checks.x86_64-linux.default --extra-experimental-features "nix-command flakes"

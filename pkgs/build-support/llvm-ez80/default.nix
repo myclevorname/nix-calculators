68060816d4dfc0bf5df432ccb1480ed0f845dae2
Sam Connelly <myclevorname@gmail.com>
2025-09-12 22:33:34 -0400

    Update llvm-ez80

--- pkgs/build-support/llvm-ez80/default.nix
+++ pkgs/build-support/llvm-ez80/default.nix
@@ -8,7 +8,7 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "llvm-ez80";
-  version = "nightly-unstable-2025-06-21";
+  version = "nightly-unstable-2025-09-11";
 
   nativeBuildInputs = [
     cmake
@@ -19,8 +19,8 @@ stdenv.mkDerivation (finalAttrs: {
     # owner = "jacobly0";
     owner = "CE-Programming";
     repo = "llvm-project";
-    rev = "db91fd729cd188daf925aaf5043a6ca0d3660655";
-    hash = "sha256-FFI3vW4O4Hyx4phzkXzId8Uv1mGdKMzNccUxGZemZtg=";
+    rev = "e04f59c2606713a1b7dfd12516ec438809ad5522";
+    hash = "sha256-Y940We4e3mv8QdnUJ9UqKGV88IA67mT2pENtS1r0g1c=";
   };
 
   patchPhase = ''

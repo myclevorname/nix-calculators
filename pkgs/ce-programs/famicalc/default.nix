5ac820d31c0b291a10c3d5bc941859ed1eac0316
Sam Connelly <myclevorname@gmail.com>
2025-10-03 14:43:44 -0400

    Update famicalc

--- pkgs/ce-programs/famicalc/default.nix
+++ pkgs/ce-programs/famicalc/default.nix
@@ -5,12 +5,12 @@
 }:
 buildCEProgram {
   pname = "famicalc";
-  version = "0-unstable-2025-09-24";
+  version = "0-unstable-2025-10-03";
   src = fetchFromGitHub {
     owner = "Zaalan3";
     repo = "Famicalc";
-    rev = "66cae3c87cd992ea1fd42ec23df53bfa82f596de";
-    hash = "sha256-cHRorCDMsqWx0GNXi/De+lFH/HAz0cDLFCeuddoOF1o=";
+    rev = "faf2c2f0a9b25c88c9499fa1cf7b84505ceb9c33";
+    hash = "sha256-69Dhf6ADDpmRl1FWnxl8py989z0eOHKnePXmtSs6aEY=";
   };
   meta = {
     description = "WIP NES emulator for the TI84+CE";

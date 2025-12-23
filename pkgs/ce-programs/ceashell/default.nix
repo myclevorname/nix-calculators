5081e2c142d7c2d8a8ff9f422a3dc6106b6c05f1
Sam Connelly <myclevorname@gmail.com>
2025-12-23 18:12:50 -0500

    Update CEaShell

--- pkgs/ce-programs/ceashell/default.nix
+++ pkgs/ce-programs/ceashell/default.nix
@@ -1,18 +1,18 @@
 {
-  buildCEProgramStable,
+  buildCEProgram,
   fetchFromGitHub,
   lib,
   python3,
 }:
-buildCEProgramStable {
+buildCEProgram {
   pname = "ceashell";
-  version = "2.0.2-unstable-2025-11-03";
+  version = "2.0.2-unstable-2025-12-23";
   src = fetchFromGitHub {
     owner = "RoccoLoxPrograms";
     repo = "CEaShell";
     fetchSubmodules = true;
-    rev = "fae90006b7540cda415a4f04bef90285072e08ce";
-    hash = "sha256-MietApzqH1q25me81YWE/BthkFEt9gKkrxbH/PAgYyY=";
+    rev = "973e7af8bd987e4ac04341186549c63294769864";
+    hash = "sha256-X8gAb6rSjj3Z7yZXHrknbsVbqBJ74OL3vuYqGPFw2MA=";
   };
   nativeBuildInputs = [
     python3
@@ -21,6 +21,5 @@ buildCEProgramStable {
     homepage = "https://ceme.tech/t18820";
     description = "CEaShell (pronounced like \"Seashell\") is a new shell for the TI-84 Plus CE.";
     license = with lib.licenses; [ gpl3Only ];
-    broken = true;
   };
 }

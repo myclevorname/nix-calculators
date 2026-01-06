96183c318c5792f885eb597384ecb1c35ce35377
Sam Connelly <myclevorname@gmail.com>
2026-01-06 18:29:11 -0500

    Fix HexaEdit, Oiram

--- pkgs/ce-programs/hexaedit/default.nix
+++ pkgs/ce-programs/hexaedit/default.nix
@@ -12,8 +12,10 @@ buildCEProgram {
     rev = "d34b22afb1b8d52f8c6d7658f9bb10a4a9230cec";
     hash = "sha256-k4WvdFvxv7brAJ5mdrF+qECTyjATbARJJxddq+WlXl4=";
   };
+
+  patches = [ ./binutils.patch ];
+
   meta = {
-    broken = true;
     description = "HexaEdit CE is a powerful on-calc hex editor for the TI-84 Plus CE.";
     license = with lib.licenses; [ bsd3 ];
   };
--- pkgs/ce-programs/oiram/default.nix
+++ pkgs/ce-programs/oiram/default.nix
@@ -12,8 +12,8 @@ buildCEProgram {
     rev = "ba2db485984ee4460e7320ab46ba5d009781786a";
     hash = "sha256-oXh9D+R3Rp1WCYykQm+r6C1MVNjSMwNaTqsh2Np3mkQ=";
   };
+  patches = [ ./binutils.patch ];
   meta = {
-    broken = true;
     description = "Oiram is a mario-style platformer programmed from scratch in C and assembly using the CE development toolchain.";
     license = with lib.licenses; [ bsd3 ];
   };

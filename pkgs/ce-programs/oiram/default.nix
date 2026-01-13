889f501a90ab29e063efe6f31db37db7d0e46fc7
Sam Connelly <myclevorname@gmail.com>
2026-01-13 06:04:59 -0500

    Update Oiram

--- pkgs/ce-programs/oiram/default.nix
+++ pkgs/ce-programs/oiram/default.nix
@@ -5,14 +5,13 @@
 }:
 buildCEProgram {
   pname = "oiram";
-  version = "0-unstable-2022-06-16";
+  version = "0-unstable-2026-01-13";
   src = fetchFromGitHub {
     owner = "mateoconlechuga";
     repo = "oiram";
-    rev = "ba2db485984ee4460e7320ab46ba5d009781786a";
-    hash = "sha256-oXh9D+R3Rp1WCYykQm+r6C1MVNjSMwNaTqsh2Np3mkQ=";
+    rev = "85d124f394c23d4ee0f5fd0235aef6275879c7b9";
+    hash = "sha256-RCPrtmhb/+1oIsiQbxoaP8F0dB9ZRBRZcb4k7BkpnQQ=";
   };
-  patches = [ ./binutils.patch ];
   meta = {
     description = "Oiram is a mario-style platformer programmed from scratch in C and assembly using the CE development toolchain.";
     license = with lib.licenses; [ bsd3 ];

af6a019ca3c340b0d585fa8c91ef1a8f9a857940
Sam Connelly <myclevorname@gmail.com>
2026-01-26 22:06:47 -0500

    Update Screensaver CE

--- pkgs/ce-programs/screensaver/default.nix
+++ pkgs/ce-programs/screensaver/default.nix
@@ -6,12 +6,12 @@
 }:
 buildCEProgramStable {
   pname = "screensaver";
-  version = "1.0-unstable-2026-01-16";
+  version = "1.0-unstable-2026-01-26";
   src = fetchFromGitHub {
     owner = "EzCE";
     repo = "ScreenSaver-CE";
-    rev = "333207c2f484c726243b098c3d5372c185167826";
-    hash = "sha256-FYj+Vfh2qKGbwaks3gGr/8blobWkwtO1o4/JIbj/nBo=";
+    rev = "ceaaf41874ac69f234e32d41c1c1af4ed0c25547";
+    hash = "sha256-fcJAgdIGWD7v02PDW1MPCYuONxCRCKeiROewlJJeNjk=";
   };
   nativeBuildInputs = [ python3 ];
   preInstall = ''
